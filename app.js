7570a94d7ef96743f905f9865f0187fe96ba5af8
jakre732 <jakre732@student.liu.se>
2024-05-23 17:19:28 +0200

    Added VS sign

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const scoreMap = new Map();
 const imageMap = new Map();
 
+let leftAnimeScore;
+let rightAnimeScore;
+
 function loadMap() {
     scoreMap.set("Attack on titan", "8.48");
     scoreMap.set("One piece", "9.08");
@@ -20,15 +23,28 @@ function loadMap() {
 }
 
 function onHigherClick() {
-    console.log("Higher");
+
+    // Check result - if they are equal, then the user wins
+    if (leftAnimeScore <= rightAnimeScore) {
+        // If we get in here, the user answered correctly
+        document.getElementById("rightScore").innerHTML = rightAnimeScore;
+    } else {
+        console.log("Incorrect");
+    }
 }
 
 function onLowerClick() {
-    console.log("Lower");
+
+    // Check result - if they are equal, then the user wins
+    if (leftAnimeScore >= rightAnimeScore) {
+        // If we get in here, the user answered correctly
+        document.getElementById("rightScore").innerHTML = rightAnimeScore;
+    } else {
+
+    }
 }
 
 function onStartClick() {
-    console.log("Start");
     
     // Get elements with class "leftAnime" and "rightAnime"
     let leftAnimeElements = document.getElementsByClassName("leftAnime");
@@ -41,6 +57,10 @@ function onStartClick() {
     for (let i = 0; i < rightAnimeElements.length; i++) {
         rightAnimeElements[i].style.opacity = 1;
     }
+
+    // Remove the start screen class
+    let div = document.getElementById("start");
+    div.parentNode.removeChild(div);
 }
 
 // returns random key from Set or Map
@@ -53,14 +73,35 @@ document.addEventListener('DOMContentLoaded', (event) => {
     // Gets called when the page gets loaded
     loadMap();
 
+    // Generate left image
     // Get random entries from maps
     let anime = getRandomKey(scoreMap);
     let score = scoreMap.get(anime);
     let image = imageMap.get(anime);
     
-    // Set the image and score to right anime
+    // Set the title, image and score to left anime
     document.getElementById("leftTitle").innerHTML = anime;
     document.getElementById("leftScore").innerHTML = score;
     document.getElementById("leftImage").src = "images/" + image;
+
+    score = parseFloat(score);
+    leftAnimeScore = score;
+
+    // Generate right image    
+    // Make new map and remove already picked entry
+    let oldAnime = anime;
+    let tempMap = scoreMap;
+    tempMap.delete(oldAnime);
+    anime = getRandomKey(tempMap);
+    score = tempMap.get(anime);
+    image = imageMap.get(anime);
+
+    // Set title and image, but not score, to right anime
+    document.getElementById("rightTitle").innerHTML = anime;
+    document.getElementById("rightScore").innerHTML = "asdf";
+    document.getElementById("rightImage").src = "images/" + image;
+
+    score = parseFloat(score);
+    rightAnimeScore = score;
 
 });
